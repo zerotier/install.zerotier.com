848b48139d3a27bf00bd1f176446a7608987059f
Adam Ierymenko <adam.ierymenko@gmail.com>
2016-07-05 22:04:38 -0700

    .

--- build-install.sh
+++ build-install.sh
@@ -3,7 +3,7 @@
 gpg --digest-algo SHA256 --clearsign install.sh.in
 
 echo '#!/bin/bash' >install.sh
-echo '<<ENDOFSIGSTART' >>install.sh
+echo '<<ENDOFSIGSTART=' >>install.sh
 cat install.sh.in.asc >>install.sh
 
 rm -f install.sh.in.asc
